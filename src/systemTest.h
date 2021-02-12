9f312f928fe64a7748712b8bef8c35df5a0c25c6
Divelix <divelix1@gmail.com>
2021-02-12 13:29:00 +0300

    added systemTest file

--- src/systemTest.h
+++ src/systemTest.h
@@ -1,10 +1,12 @@
 #include <PinChangeInterrupt.h>
+#include <Thread.h>
 #include "Car.h"
+#include "SerialTransceiver.h"
 
 float desiredRpms[4] = {};  // init with zeros
 float feedbackRpms[4] = {}; // init with zeros
 
-int carPeriod = 40; // millis
+int carPeriod = 40;
 int encoderPins[8] = {18, 19, 20, 21, 50, 52, 51, 53};
 Car car(encoderPins, carPeriod, desiredRpms, feedbackRpms);
 void updateW1A() { car.incEnc1A(); }
@@ -15,11 +17,18 @@ void updateW3A() { car.incEnc3A(); }
 void updateW3B() { car.incEnc3B(); }
 void updateW4A() { car.incEnc4A(); }
 void updateW4B() { car.incEnc4B(); }
+SerialTransceiver transceiver(desiredRpms, feedbackRpms);
+
+Thread carThread = Thread();
+Thread serialThread = Thread();
+
+void updateCar() { car.update(); }
+void talkSerial() { transceiver.talk(); }
 
 void setup()
 {
     attachInterrupt(digitalPinToInterrupt(car.getEncPin1A()), updateW1A, RISING);
-    attachInterrupt(digitalPinToInterrupt(car.getEncPin1B()), updateW1B, RISING);
+    attachPCINT(digitalPinToPCINT(car.getEncPin1B()), updateW1B, RISING);
     attachInterrupt(digitalPinToInterrupt(car.getEncPin2A()), updateW2A, RISING);
     attachInterrupt(digitalPinToInterrupt(car.getEncPin2B()), updateW2B, RISING);
     attachPCINT(digitalPinToPCINT(car.getEncPin3A()), updateW3A, RISING);
@@ -29,9 +38,19 @@ void setup()
 
     Serial.begin(115200);
     Serial.setTimeout(1000);
+
+    carThread.onRun(updateCar);
+    serialThread.onRun(talkSerial);
 }
 
 void loop()
 {
-    car.update();
+    if (carThread.shouldRun())
+    {
+        carThread.run();
+    }
+    if (serialThread.shouldRun())
+    {
+        serialThread.run();
+    }
 }
